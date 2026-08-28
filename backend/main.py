863ef1a999e39fa35c684ec0d5b4560a09e6a6ee
Sunny Singh <sunnysinghforeverat@gmail.com>
2026-08-28 12:07:48 +0530

    Fix production CORS

--- backend/main.py
+++ backend/main.py
@@ -28,12 +28,15 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=False,
+    allow_origins=[
+        "https://private-chat-apllication-2.onrender.com",
+        "http://localhost:5173",
+        "http://127.0.0.1:5173"
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 app.include_router(chat_router)
 
